f30ad3950d2dc352d675c6adb0636a5782d04fc2
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-10-26 21:20:11 +0000

    assert.js tested and working

--- assertEqual.js
+++ assertEqual.js
@@ -1,1 +1,14 @@
-console.log(`is this thing on?`)
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(2, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(4, (1 + 3));
