c225d211fda91cb310091e5277602191b85d7375
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-10-26 21:49:52 +0000

    tail.js linted

--- tail.js
+++ tail.js
@@ -9,11 +9,11 @@ const assertEqual = function(actual, expected) {
 const tail = function(array) {
   if (array.length === 0) {
     return [];
-  };
+  }
   return array.slice(1);
 };
 
-// Test Case: Check the original array 
+// Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
