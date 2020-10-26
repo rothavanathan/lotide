f0b9e0f131699bb3ba0a89cfa9e3cdb4687ead3b
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-10-26 21:32:19 +0000

    tests were updated!

--- head.js
+++ head.js
@@ -16,5 +16,5 @@ const head = function(array) {
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([3]), 3);
-assertEqual(head([], false));
+assertEqual(head([]), undefined);
 
