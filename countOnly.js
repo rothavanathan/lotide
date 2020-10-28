77937419d2bc2d960e7507564f0f18b7c59278b3
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-10-28 18:38:15 +0000

    countOnly.js passes assertions and linted

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    //if key is in itemsToCount && is true in our itemsToCount object
+    if ((Object.keys(itemsToCount).indexOf(item) !== -1) && itemsToCount[item] === true) {
+      if (results[item]) {
+        results[item]++;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+
+
+//TEST CODE
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+console.log("result1 is: ", result1);
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
