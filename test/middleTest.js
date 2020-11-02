c8deca7f63ed3c8a02a1de7b0c0fd9bcf49e7160
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-02 19:13:38 +0000

    middle.js and tail.js - converted to mocha framework and chai library for testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,25 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
-const eqArrays = require('../eqArrays');
 
-// TEST CODE
-const testArray = [1, 2, 3, 4, 5];
-const testArray2 = [1, 2];
-const testArray3 = [1, 2, 3, 4, 5, 6];
-assertArraysEqual(eqArrays(middle(testArray), [3]), true);
-assertArraysEqual(eqArrays(middle(testArray2), []), true);
-assertArraysEqual(eqArrays(middle(testArray3), [3, 4]), true);
-assertArraysEqual(eqArrays(middle(testArray3), [3]), false);
+describe("middle", () => {
+
+  it("should return empty for arrays with one element", () => {
+    const result = middle([1]);
+    assert.deepEqual(result, []);
+  });
+  
+  it("should return empty for arrays with two elements", () => {
+    const result = middle([1, 2]);
+    assert.deepEqual(result, []);
+  });
+
+  it("should return an array with two elements for arrays with an even number of elements", () => {
+    const result = middle([1, 2, 3, 4]);
+    assert.deepEqual(result, [2, 3]);
+  });
+
+  it("should return an array with one element for arrays with an odd number of elements", () => {
+    const result = middle([1, 2, 3, 4, 5]);
+    assert.deepEqual(result, [3]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,17 +1,30 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const result = tail(words); // no need to capture the return value since we are not checking it
-assertEqual(result.length, 2); // original array should still have 3 elements!
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("tail", () => {
 
+  it("should return everything but the first item of an array when the array has more than one item", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    const result = tail(words);
+    assert.deepEqual(result, ['Lighthouse', 'Labs']);
+  });
 
-//test: one element array
-const songs = ["I'll believe in anything"];
-assertEqual(tail(songs).length, 0);
+  it("should not mutate original array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.deepEqual(words, ['Yo Yo', 'Lighthouse', 'Labs']);
+  });
 
-//test: empty array
-const empty = [];
-assertEqual(tail(empty).length, 0);
+  it("should return an empty array when passed an array with one element", () => {
+    const words = ["Labs"];
+    const result = tail(words);
+    assert.deepEqual(result, []);
+  });
+
+  it("should return an empty array when passed an empty array", () => {
+    const words = [""];
+    const result = tail(words);
+    assert.deepEqual(result, []);
+  });
+    
+});
