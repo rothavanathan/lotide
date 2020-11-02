928e160bd7b0ee1db7cb0f1e6f77022e3bb46890
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-02 18:38:26 +0000

    headTest.js - changed testing to use mocha frameowrk and chai functions

--- head.js
+++ head.js
@@ -1,7 +1,12 @@
 //head() takes an array as a parameter and returns the first element
+//if array is empty will return undefined
 const head = function(array) {
+  if (array.length === 0) {
+    return undefined;
+  }
   return array[0];
 };
+
 
 module.exports = head;
 
--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,21 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const expect = require('chai').expect;
 const head = require('../head');
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([3]), 3);
-assertEqual(head([]), undefined);
+describe("#head", () => {
+
+  it("returns 1 for [1, 2, 3]", () => {
+    const result = head([1,2,3]);
+    assert.strictEqual(result, 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns undefined for an empty array", () => {
+    const result = head([]);
+    expect(result).to.be.undefined;
+  });
+});
+
