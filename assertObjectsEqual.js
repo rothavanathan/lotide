18f94600b1dd6550b8cea468be997caaad7aa873
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-02 19:48:20 +0000

    assertObjectsEqual.js - module exported and testing moved to mocha framework and chai functions

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,23 +1,13 @@
-const assertEqual = require('./assertEqual');
-const eqArrays = require('./eqArrays');
-const eqObjects = require('./eqObjects');
-
-
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-
 // Implement assertObjectsEqual which will take in two objects and console.log an appropriate message to the console.
 // FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(test, expectation) {
   if ((test && expectation === true) || (!test && expectation === false)) {
     console.log(`✅✅✅ Assertion Passed: your objects are ${expectation === true ? "equal" : "not equal"}, just like you thought!!`);
+    return true;
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: your objects are ${expectation === true ? "not equal!!" : "equal!!"}`);
+    return false;
   }
 };
 
-const dict1 = {a:1, b:[2,3]};
-const dict2 = {b:[2,3], a:1};
-const dict3 = {a:2};
-assertObjectsEqual(eqObjects(dict1, dict2), true);
-assertObjectsEqual(eqObjects(dict1, dict3), false);
+module.exports = assertObjectsEqual;
---
+++ test/assertObjectsEqual.js
@@ -0,0 +1,44 @@
+const assert = require('chai').assert;
+const { expect } = require('chai');
+const assertObjectsEqual = require('../assertObjectsEqual');
+const eqObjects = require('../eqObjects');
+
+describe("tail", () => {
+
+  it("should return true if both objects are identical and expectation is true", () => {
+    const dict1 = {a:1, b:[2,3]};
+    const dict2 = {b:[2,3], a:1};
+    const result = assertObjectsEqual(eqObjects(dict1, dict2),true);
+    expect(result).to.be.true;  
+  });
+
+  it("should return true if both objects are not identical and expectation is false", () => {
+    const dict1 = {a:1, b:[2,3]};
+    const dict3 = {a:2};
+    const result = assertObjectsEqual(eqObjects(dict1, dict3), false);
+    expect(result).to.be.true;  
+  });
+
+  it("should return false if both objects are identical and expectation is false", () => {
+    const dict1 = {a:1, b:[2, 3]};
+    const dict2 = {a:1, b: [2, 3]};
+    const result = assertObjectsEqual(eqObjects(dict1, dict2), false);
+    expect(result).to.be.false;  
+  });
+
+  it("should return false if both objects are not identical and expectation is true", () => {
+    const dict1 = {a:1, b:[2,3]};
+    const dict3 = {a:2};
+    const result = assertObjectsEqual(eqObjects(dict1, dict3), true);
+    expect(result).to.be.false;  
+  });
+    
+  // Returns true if both objects have identical keys with identical values.
+  
+  // Otherwise you get back a big fat false!
+  
+  
+  // assertObjectsEqual(eqObjects(dict1, dict2), true);
+  // assertObjectsEqual(eqObjects(dict1, dict3), false);
+});
+
