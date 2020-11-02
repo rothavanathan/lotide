52a2150112bcd4ba2bb7209aeac54984fd2a6de7
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-02 17:50:54 +0000

    added mocha and chai. separated testing for assertEqual and head into separate test folder.

--- assertEqual.js
+++ assertEqual.js
@@ -8,11 +8,3 @@ const assertEqual = function(actual, expected) {
 };
 
 module.exports = assertEqual;
-
-// // TEST CODE
-// //passing
-// assertEqual("Lighthouse Labs", "Lighthouse Labs");
-// assertEqual(4, (1 + 3));
-// //failing
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(2, 1);
